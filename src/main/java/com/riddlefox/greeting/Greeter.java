1264bffa17d33eedda5dcb3a335e1851e5e46034
James Burt <james@orbific.com>
2016-03-18 06:35:47 +0000

    Added jacoco to gradle build, removed main method from Greeter

--- src/main/java/com/riddlefox/greeting/Greeter.java
+++ src/main/java/com/riddlefox/greeting/Greeter.java
@@ -1,10 +1,6 @@
 package com.riddlefox.greeting;
 
 public class Greeter {
-    public static void main (String[] args) {
-        System.out.println(greet("world"));
-    }
-
     public static String greet(String name) {
         return "Hello " + name;
     }
