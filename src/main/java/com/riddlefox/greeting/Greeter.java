a57f3e105ab9a8912285dcb2e38f1c6104e43cc2
James Burt <james@orbific.com>
2016-04-09 13:42:48 +0100

    temp commit

--- src/main/java/com/riddlefox/greeting/Greeter.java
+++ src/main/java/com/riddlefox/greeting/Greeter.java
@@ -1,7 +1,23 @@
 package com.riddlefox.greeting;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
 public class Greeter {
+
+	@RequestMapping("/")
     public static String greet(String name) {
+    	if(name==null) {
+    		return "Hello World";
+    	}
         return "Hello " + name;
+    }
+    
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(Greeter.class, args);
     }
 }
--- src/test/java/com/riddlefox/greeting/GreeterTest.java
+++ src/test/java/com/riddlefox/greeting/GreeterTest.java
@@ -14,4 +14,13 @@ public class GreeterTest {
       String response = greeter.greet("World");
       assertEquals("String should be as expected", "Hello World", response);
    }
+   
+   @Test
+   public void testHelloWorldNameNull() 
+   {
+      Greeter greeter = new Greeter();
+      String response = greeter.greet(null);
+      assertEquals("String should be as expected", "Hello World", response);
+   }
+
 }
